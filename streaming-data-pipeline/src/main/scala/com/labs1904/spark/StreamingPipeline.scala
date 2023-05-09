32abd5f3a8ca42fcbd0cef0fcb8dcfa76f3d8ebc
Robert Swyers <rswyers08@gmail.com>
2023-05-08 20:54:00 -0500

    final project

--- streaming-data-pipeline/src/main/scala/com/labs1904/spark/StreamingPipeline.scala
+++ streaming-data-pipeline/src/main/scala/com/labs1904/spark/StreamingPipeline.scala
@@ -1,7 +1,10 @@
 package com.labs1904.spark
 
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{ConnectionFactory, Get}
+import org.apache.hadoop.hbase.util.Bytes
 import org.apache.log4j.Logger
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
 
@@ -13,18 +16,22 @@ object StreamingPipeline {
   lazy val logger: Logger = Logger.getLogger(this.getClass)
   val jobName = "StreamingPipeline"
 
-  val hdfsUrl = System.getenv("HBASE")
+  val hdfsUrl = "CHANGEME"
   val bootstrapServers = System.getenv("BOOTSTRAP")
   val username = System.getenv("USERNAME")
   val password = System.getenv("PASSWORD")
-  val hdfsUsername = System.getenv("HANDLE")
+  val hdfsUsername = "CHANGEME"
 
   //Use this for Windows
-  val trustStore: String = "src\\main\\resources\\kafka.client.truststore.jks"
+  //val trustStore: String = "src\\main\\resources\\kafka.client.truststore.jks"
   //Use this for Mac
-  //val trustStore: String = "src/main/resources/kafka.client.truststore.jks"
+  val trustStore: String = "src/main/resources/kafka.client.truststore.jks"
+
+  case class User(user_id: String, mail: String)
+  case class Review(marketplace: String, customer_id: String, review_id: String, product_id: String, product_parent: String, product_title: String, product_category: String, star_rating: String, helpful_votes: String, total_votes: String, vine: String, verified_purchase: String, review_headline: String, review_body: String, review_date: String)
 
   def main(args: Array[String]): Unit = {
+
     try {
       val spark = SparkSession.builder()
         .config("spark.sql.shuffle.partitions", "3")
@@ -51,15 +58,32 @@ object StreamingPipeline {
 
       // TODO: implement logic here
       val result = ds
-      result.printSchema()
+      //result.printSchema()
+
+
+      val results = result.flatMap(x=> x.split("\t"))
+      val reviews = results.map(x=> Review(x(0).toString, x(1).toString, x(2).toString,x(3).toString,x(4).toString,x(5).toString,x(6).toString,x(7).toString,x(8).toString,x(9).toString,x(10).toString,x(11).toString,x(12).toString,x(13).toString,x(14).toString))
+      val mappedReviews = reviews.mapPartitions(partition=> {
+        val conf = HBaseConfiguration.create()
+        conf.set("hbase.zookeeper.quorum", System.getenv("HBASE"))
+        val connection = ConnectionFactory.createConnection(conf)
+        val table = connection.getTable(TableName.valueOf("bswyers:users"))
+
+        val iter = partition.map(review => {
+          val get = new Get(Bytes.toBytes(review.customer_id)).addFamily(Bytes.toBytes("f1"))
+          val result = table.get(get)
+          new User(review.customer_id, Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail"))))
+        })
+      }).toList.iterator
+
       // Write output to console
-      val query = result.writeStream
+      val query = results.writeStream
         .outputMode(OutputMode.Append())
         .format("console")
         .option("truncate", false)
         .trigger(Trigger.ProcessingTime("5 seconds"))
         .start()
-
+      //output should be case class, not tuple
       // Write output to HDFS
 //      val query = result.writeStream
 //        .outputMode(OutputMode.Append())
@@ -79,4 +103,7 @@ object StreamingPipeline {
    username=\"$username\"
    password=\"$password\";"""
   }
+//  def stringConverter(review: Dataset[String]) : Review = {
+//    //Review(review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString, review(0).toString)
+//  }
 }
