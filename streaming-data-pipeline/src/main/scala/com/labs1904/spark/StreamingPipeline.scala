8bac3163424189da3702f9041fde0fc32ea92348
Baubs <pathogenic@gmail.com>
2023-05-21 23:32:49 -0500

    Comment cleanup

--- streaming-data-pipeline/src/main/scala/com/labs1904/spark/StreamingPipeline.scala
+++ streaming-data-pipeline/src/main/scala/com/labs1904/spark/StreamingPipeline.scala
@@ -31,7 +31,6 @@ object StreamingPipeline {
   case class User(mail: String, birthdate: String, name: String, sex: String, username: String)
   case class EnrichedReview(user_id: String, mail: String, marketplace: String, review_id: String, product_id: String, product_parent: String, product_title: String, product_category: String, username: String, helpful_votes: String, total_votes: String, vine: String, verified_purchase: String, review_headline: String, review_body: String, review_date: String, birthdate: String, name: String, sex: String, star_rating: String)
   case class Review(marketplace: String, customer_id: String, review_id: String, product_id: String, product_parent: String, product_title: String, product_category: String, review_date: String, helpful_votes: String, total_votes: String, vine: String, verified_purchase: String, review_headline: String, review_body: String, star_rating: String)
-  //case class TestReview(user: User, review: Review, star_rating: String);
 
   def main(args: Array[String]): Unit = {
 
@@ -67,23 +66,23 @@ object StreamingPipeline {
         .option("kafka.sasl.jaas.config", getScramAuthString(username, password))
         .load()
         .selectExpr("CAST(value AS STRING)").as[String]
-
+      //Raw review data from kafka topic
       val result = ds
 
-
-      //val results = result.map(x => x.split('\n'))
-      //val reviewList = results.map(x => x(0).split('\t'))
+      //Splitting the review strings into arrays and then transforming them into a review case class
       val reviewList = result.map(x=> x.split('\t'))
-      //val reviews = reviewList.map(x=> Review(x(0), x(1), x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),x(10),x(11),x(12),x(13),x(14)))
       val reviews = reviewList.map(x=> reviewBuilder(x))
+
+      //Using the customer IDs from the reviews to get the user information from hbase
       val enrichedReviews = reviews.mapPartitions(partition=> {
         val conf = HBaseConfiguration.create()
         conf.set("hbase.zookeeper.quorum", System.getenv("HBASE"))
         val connection = ConnectionFactory.createConnection(conf)
         val table = connection.getTable(TableName.valueOf("bswyers:users"))
 
-        val iter = partition.map(review => {
+        val partitionedReviews = partition.map(review => {
           val get = new Get(Bytes.toBytes(review.customer_id)).addFamily(Bytes.toBytes("f1"))
+          //Code to figure out all the field types of the user data from hbase
 //          val debugScan = new Scan().setOneRowLimit()
 //          val scanResult = table.getScanner(debugScan)
 //          val scanList = scanResult.iterator()
@@ -91,17 +90,12 @@ object StreamingPipeline {
           val result = table.get(get)
           val user = userBuilder(result)
           enrichedReviewBuilder(user, review)
-          //logger.debug(Bytes.toString(result.value))
-          //EnrichedReview(review.customer_id, Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("mail"))), review.marketplace, review.review_id, review.product_id, review.product_parent, review.product_title, review.product_category,Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("username"))), review.helpful_votes, review.total_votes, review.vine, review.verified_purchase, review.review_headline, review.review_body, review.review_date, Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("birthdate"))), Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("name"))), Bytes.toString(result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("sex"))), review.star_rating)
-          //TestReview looks nicer in code but would also require changing format spark writes to.
-          //TestReview(userBuilder(result), review, review.star_rating)
         }).toList.iterator
 
         connection.close()
-        iter
+        partitionedReviews
       })
 
-      //mappedReviews.printSchema()
       // Write output to console
 //      val query = enrichedReviews.writeStream
 //        .outputMode(OutputMode.Append())
@@ -109,9 +103,8 @@ object StreamingPipeline {
 //        .option("truncate", false)
 //        .trigger(Trigger.ProcessingTime("5 seconds"))
 //        .start()
-      //output should be case class, not tuple
-      // Write output to HDFS
 
+      // Write output to HDFS
     val query = enrichedReviews.writeStream
       .outputMode(OutputMode.Append())
       .format("csv")
@@ -133,6 +126,7 @@ object StreamingPipeline {
    password=\"$password\";"""
   }
 
+  //Helper functions to build the case classes and make it more readable.
   def reviewBuilder(review: Array[String]): Review = {
     Review(
       marketplace = review(0),
